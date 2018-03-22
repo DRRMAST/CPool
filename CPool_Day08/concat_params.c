5c358abcbb5f2eb519ce8b76ca0d4e714e2191b6
DRRMAST <heztzxy@yeah.net>
2018-03-22 22:16:56 +0800

    fitst

---
+++ CPool_Day08/concat_params.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+char *my_strcat(char *,char const*);
+int my_putstr(char *);
+int my_strlen(char const*);
+char *concat_params ( int argc , char **argv )
+{
+	int len = 0;
+	int i  = 0;
+	char *re = NULL;
+	while(i < argc){
+		len = len + my_strlen(argv[i]);
+		i++;
+	}
+	i = 0;
+	len = len + 5;
+	re = malloc(len);
+	while(i < argc){
+		re = my_strcat(re,argv[i]);
+		if(i < argc - 1)
+			re = my_strcat(re,"\n");
+		if(i == argc - 1)
+			re = my_strcat(re,"\0");
+		i++;
+	}
+	return re;
+}
+
